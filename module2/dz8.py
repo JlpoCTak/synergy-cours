5a1754313c7e4643492f76ce9af21b32dbb6dc7f
JlpoCTak <JlpocTaK@yandex.ru>
2024-01-10 16:42:47 +0300

    .

--- module2/dz8.py
+++ module2/dz8.py
@@ -1,24 +1,34 @@
 import numpy as np
 
-def check7(x):
-    if x > 7:
-        return True
 
 def zadacha1():
     arr = np.random.randint(1, 11, size=100)
     print(arr)
     print()
-    check_arr = np.apply_along_axis(check7, axis=0, arr=arr)
+    check_arr = np.mean(arr > 7) * 100
 
-    print(check_arr)
+    print("Ответ: ", check_arr, '%')
 
 
 def zadacha2():
-    pass
+    proverka = 0
+    for i in range(1001):
+        arr = np.random.randint(1, 11, size=100)
+        check_arr = np.mean(arr > 7) * 100
+        if check_arr == 20:
+            proverka += 1
+    print(f"Ответ: {proverka}/1000")
 
 
 def zadacha3():
-    pass
+    arr = np.tile(np.arange(1, 11), (10, 1))
+    print(arr)
+
+
+def zadacha4():
+    arr = np.tile(np.arange(1, 11), (10, 1))
+    sum_arr_y = arr.sum(axis=0)
+    print(sum_arr_y)
 
 
 def main():
@@ -27,7 +37,8 @@ def main():
               '\n1)задача1'
               '\n2)задача2'
               '\n3)задача3'
-              '\n4+)выход')
+              '\n4)задача4'
+              '\n5+)выход')
         a = input()
         if a == '1':
             zadacha1()
@@ -35,6 +46,8 @@ def main():
             zadacha2()
         elif a == '3':
             zadacha3()
+        elif a == '4':
+            zadacha4()
         else:
             break
 
