e0b804754d189d8307477b4f636eaec0aec82b3e
JlpoCTak <JlpocTaK@yandex.ru>
2024-01-12 21:53:33 +0300

    Update matplotlib_2.py

--- module2/matplotlib_2.py
+++ module2/matplotlib_2.py
@@ -15,15 +15,19 @@ def parable(x):
 # plt.plot(x,y)
 # plt.show()
 
-x = np.linspace(1, 10, 300)
-y = parable(x)
-plt.plot(x, y)
-plt.grid()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Parable')
-plt.show()
+# x = np.linspace(1, 10, 300)
+# y = parable(x)
+# plt.plot(x, y)
+# plt.grid()
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Parable')
+# plt.show()
 
+x = np.linspace(0, 6, 100)
+plt.figure(figsize=(10, 5))
+plt.plot(x, np.sin(x), linewidth=2, color='g', dashes=[8, 4, 2, 4], label='sin(x)', alpha=0.5)
+plt.show()
 
 
 
