d352ac8ab678b33f0451fc88b9a5f8548423c024
JlpoCTak <JlpocTaK@yandex.ru>
2024-01-14 18:41:02 +0300

    .

---
+++ module2/dz12.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from math import sqrt
+
+
+def func(x):
+    return (2*np.cos(x))/(sqrt(1+np.tan(x)**2))
+
+
+def zadacha1():
+    xlist = np.linspace(-10, 10, 100)
+    y = [func(x) for x in xlist]
+    plt.figure(figsize=(10, 5))
+    plt.plot(xlist, y, linewidth=2, color='g', dashes=[8, 4], label='Вот такая моя функция', alpha=0.5)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Титульник')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+def zadacha2():
+    x = np.random.normal(0, 1, 3000)
+    y = np.random.normal(3, 4, 3000)
+    color = np.random.rand(3000)
+    sizes = 100 * np.random.rand(x.shape[0])
+    plt.scatter(x, y, c=color, s=sizes, alpha=0.5, marker='<')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+def zadacha3():
+    data = np.random.normal(16, 2, 1000)
+    plt.hist(data, bins=20, color='g')
+    plt.show()
+    #вывод: большенство значений стремится к середине
+
+
+def main():
+    while True:
+        print('Введите номер дейстивия'
+              '\n1)задача1'
+              '\n2)задача2'
+              '\n3)задача3'
+              '\n4+)выход')
+        a = input()
+        if a == '1':
+            zadacha1()
+        elif a == '2':
+            zadacha2()
+        elif a == '3':
+            zadacha3()
+        else:
+            break
+
+
+main()
--- module2/matplotlib_2.py
+++ module2/matplotlib_2.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 # plt.plot([1, 2, -1],[3, 7, 9])
@@ -24,10 +25,39 @@ def parable(x):
 # plt.title('Parable')
 # plt.show()
 
-x = np.linspace(0, 6, 100)
-plt.figure(figsize=(10, 5))
-plt.plot(x, np.sin(x), linewidth=2, color='g', dashes=[8, 4, 2, 4], label='sin(x)', alpha=0.5)
+# x = np.linspace(0, 6, 100)
+# plt.figure(figsize=(10, 5))
+# plt.plot(x, np.sin(x), linewidth=2, color='g', dashes=[8, 4, 2, 4], label='sin(x)', alpha=0.5)
+# plt.show()
+
+# plt.plot(x, np.cos(x),  linewidth=2, color='r', dashes=[8, 4, 2, 4], label='sin(x)', alpha=0.5)
+# plt.xticks(np.linspace(0, 6, 3), ('0', '1', '2'), fontsize=20) #подписи оси х, (по оси у .yticks)
+# plt.show()
+
+# x = np.random.rand(100) #от 0 до 1, 100 точек
+# y = np.random.rand(100)
+# color = np.random.rand(100)
+# sizes = 1000 * np.random.rand(100)
+# plt.scatter(x, y, c=color, s=sizes, alpha=0.5)
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
+
+# data = np.random.randn(1000)
+# plt.hist(data, bins=10, color='g')
+# plt.show()
+
+# categ = ['a', 'b', 'c']
+# count = [45, 209, 20]
+# # plt.bar(categ, count)
+# plt.pie(count, labels=categ, autopct='%1.1f%%')
+# plt.show()
+
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(x**2 + y**2))
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.plot_surface(x, y, z)
 plt.show()
-
-
-
