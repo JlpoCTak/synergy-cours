3ebaafa4bdef664a7a872ee0d9dccc990e5bb50b
JlpoCTak <JlpocTaK@yandex.ru>
2024-03-16 13:59:38 +0300

    2.0

--- module3/dz3.py
+++ module3/dz3.py
@@ -14,7 +14,8 @@ w0 = 0
 w1 = 0
 w2 = 0
 
-epochs = 300
+epochs = 100
+losses = []
 for epoch in range(1, epochs + 1):
     for i in range(len(x1)):
         predict = w1 * x1[i] + w2 * x2[i] + w0
@@ -22,6 +23,20 @@ for epoch in range(1, epochs + 1):
         w2 += 2 * lr * x2[i] * (y[i] - predict)
         w0 += 2 * lr * (y[i] - predict)
 
+loss = 0
+for i in range(len(x1)):
+    loss += 1 / len(x1) * (y[i] - (w1 * x1[i] + w2 * x2[i] + w0))
+losses.append(loss)
 print(f'w0 = {w0}, w1 = {w1}, w2 = {w2}')
+# w0 = -0.6915148481153723, w1 = 2.9955375929534673, w2 = 2.0175643492358
 
-#w0 = -0.6915148481153723, w1 = 2.9955375929534673, w2 = 2.0175643492358
+print(losses[0])
+fig, ax = plt.subplots()
+ax.grid()
+plt.scatter(x1, y, color='green')
+plt.scatter(x2, y, color='red')
+plt.plot(np.arange(1, epochs+1, epochs+1), losses)
+plt.xlabel('loss')
+plt.ylabel('epoch')
+plt.grid()
+plt.show()
